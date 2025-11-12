e8ea3f1f3e017db755d8be919bbe8a1fbb47984a
Felix Gaebler <felix.gaebler@rewe-group.com>
2025-11-12 10:04:43 +0100

    added photobox specific code

---
+++ stage2/99-wayfire/01-run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Create necessary directories
+install -d "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/wayfire/lib"
+install -d "${ROOTFS_DIR}/usr/share/wayfire/metadata"
+install -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config"
+install -d "${ROOTFS_DIR}/root/.docker"
+install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
+
+# Install files
+install -m 644 -v files/libhide-cursor.so "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/wayfire/libhide-cursor.so"
+install -m 644 -v files/hide-cursor.xml "${ROOTFS_DIR}/usr/share/wayfire/metadata/hide-cursor.xml"
+install -m 644 -v files/wayfire.ini "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/wayfire.ini"
+install -m 644 -v files/config.json "${ROOTFS_DIR}/root/.docker/config.json"
+install -m 644 -v files/photobox.service "${ROOTFS_DIR}/etc/systemd/system/photobox.service"
+install -m 644 -v files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+install -m 644 -v files/docker-compose.yaml "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/docker-compose.yaml"
+install -m 644 -v files/production.db "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/production.db"
+install -m 755 -v files/autostart.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/autostart.sh"
+
+# Clear motd file
+: > "${ROOTFS_DIR}/etc/motd"
+
+# Fix ownership of user files (using chroot to ensure correct UID/GID)
+on_chroot << EOF
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.config
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/autostart.sh
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/docker-compose.yaml
+
+sudo systemctl enable photobox.service
+sudo systemctl start photobox.service
+EOF
---
+++ stage2/99-wayfire/02-run-chroot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# Install argon case
+curl https://download.argon40.com/argon-eeprom.sh | bash
+curl https://download.argon40.com/argonneo5.sh | bash
+
+# Install wayland
+apt-get install -y wayfire seatd xdg-user-dirs
+
+# Install chromium-browser
+apt-get install -y chromium
+
+# Install docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli docker-compose-plugin
